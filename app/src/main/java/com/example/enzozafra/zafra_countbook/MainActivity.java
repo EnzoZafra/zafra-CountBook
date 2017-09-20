a09130af592d34c52555b9cc29fbc9e811e8655a
enzozafra <zafra@ualberta.ca>
2017-09-20 14:59:53 -0600

    Added a new activity for a new counter

--- app/src/main/java/com/example/enzozafra/zafra_countbook/MainActivity.java
+++ app/src/main/java/com/example/enzozafra/zafra_countbook/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.enzozafra.zafra_countbook;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user clicks the "+" button */
+    public void newCounter(View view) {
+        Intent intent = new Intent(this, NewCounterActivity.class);
+        /** TODO:
+         * Add extra things? maybe? not sure...
+         */
+
+
+        startActivity(intent);
     }
 }
